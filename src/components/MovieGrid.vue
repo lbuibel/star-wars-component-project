b4bf8fa7115805b845918563d3764149bd4f1cfc
Logan Uibel <lbuibel@gmail.com>
2019-09-23 13:50:54 -0600

    set up star wars movie components

---
+++ src/components/MovieGrid.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="4" v-for="film in films"
+            :key="film.title">
+                <star-wars-movie :film="film"></star-wars-movie>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+
+
+<script>
+import StarWarsMovie from '../components/StarWarsMovie'
+
+export default {
+    props: ["films"],
+    components: {
+        StarWarsMovie
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StarWarsMovie.vue
@@ -0,0 +1,24 @@
+<template>
+        <v-card>
+            <v-card-title>
+                <div>
+                    <h3> {{ film.title }} </h3>
+                </div>
+                <div>
+                    <p> Released: {{ film.release_date }}</p>
+                </div>
+            </v-card-title>
+        </v-card>
+        
+</template>
+
+<script>
+
+export default {
+    props: ["film"],
+}
+</script>
+
+<style scoped>
+
+</style>
